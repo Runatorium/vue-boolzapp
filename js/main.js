408592d0f5e8ba11ccbbe1c45361d6ce99df906d
Runatorium <stefano.alesci1@gmail.com>
2023-03-17 17:59:55 +0100

    lista dinamica dei contatti (milestone 1) completata

--- js/main.js
+++ js/main.js
@@ -4,7 +4,68 @@ createApp(
     {
         data() {
             return {
-                
+                contacts: [
+                    {
+                        nome: "michele",
+                        img: "./img/avatar_3.jpg",
+                        status: true,
+                        messaggi:[
+                            {
+                                text:"",
+                                tipo: "sent",
+                            },
+                            {
+                                text:"",
+                                tipo: "received",
+                            }
+                        ],
+                    },
+                    {
+                        nome: "Gianni",
+                        img: "./img/avatar_4.jpg",
+                        status: true,
+                        messaggi:[
+                            {
+                                text:"",
+                                tipo: "sent",
+                            },
+                            {
+                                text:"",
+                                tipo: "received",
+                            }
+                        ],
+                    },
+                    {
+                        nome: "Gianna",
+                        img: "./img/avatar_5.jpg",
+                        status: true,
+                        messaggi:[
+                            {
+                                text:"",
+                                tipo: "sent",
+                            },
+                            {
+                                text:"",
+                                tipo: "received",
+                            }
+                        ],
+                    },
+                    {
+                        nome: "Piero",
+                        img: "./img/avatar_2.jpg",
+                        status: true,
+                        messaggi:[
+                            {
+                                text:"",
+                                tipo: "sent",
+                            },
+                            {
+                                text:"",
+                                tipo: "received",
+                            }
+                        ],
+                    }
+                ]
             } 
             
         },
